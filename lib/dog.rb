51ab06fe0e8e9d0629ed0f5ae2a7860287628c52
Kyle Talmage <ktalmage85@gmail.com>
2020-03-13 23:04:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,8 +59,12 @@ class Dog
     
     
       def self.new_from_db(row)
-        new_dog = self.new(
-        new_dog
+      attributes_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+      self.new(attributes_hash)
       end
 
       
